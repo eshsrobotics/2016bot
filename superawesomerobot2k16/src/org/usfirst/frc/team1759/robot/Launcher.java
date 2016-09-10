da4dee57c09db43ea6bfbef681edffd805d1a144
cav3johnson <leahysmichael@gmail.com>
2016-09-10 14:03:54 -0700

    Practice and demonstration robot for the fall
    -removed excess climber and launcher code
    -removed refrences to opencv
    -added tilt control for launcher talon 6
    -commented out half speed modifier myRobot.setMaxOutput(0.5);
    -added solenoid control

--- superawesomerobot2k16/src/org/usfirst/frc/team1759/robot/Launcher.java
+++ superawesomerobot2k16/src/org/usfirst/frc/team1759/robot/Launcher.java
@@ -5,13 +5,13 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 //import edu.wpi.first.wpilibj.Servo;
 public class Launcher {
-	
+
 	private CANTalon loadTalon, lowerShootTalon, upperShootTalon;
 	private Victor spinTalon; //for using 393 motor
 	//private Servo turnServo;
 	private double error; //error value
 	private double min; //hard coded shooter min and max values
-	private double max; 
+	private double max;
 	public Launcher(int loadTalonPort, int lowerShootTalonPort, int upperShootTalonPort){
 		loadTalon = new CANTalon(loadTalonPort);
 		lowerShootTalon = new CANTalon(lowerShootTalonPort);
@@ -21,7 +21,7 @@ public class Launcher {
 	}
 	public void load(Joystick joystick, double loadingSpeed, double halfSpeed){
 		if (joystick.getRawButton(5)){ //runs loading talon forwards with button 5
-		    loadTalon.set(loadingSpeed);
+			loadTalon.set(loadingSpeed);
 			System.out.println("load in full speed");
 		}
 		else if (joystick.getRawButton(6)){ //runs loading talon backwards with button 6
@@ -29,8 +29,8 @@ public class Launcher {
 			System.out.println("load out full speed");
 		}
 		else if (joystick.getRawButton(3)){ //runs loading talon forwards with button 3 at half speed
-		    loadTalon.set(halfSpeed);
-		    System.out.println("load in half speed");
+			loadTalon.set(halfSpeed);
+			System.out.println("load in half speed");
 		}
 		else if (joystick.getRawButton(4)){ //runs loading talon backwards with button 4 at half speed
 			loadTalon.set((halfSpeed*-1.0));
@@ -41,141 +41,144 @@ public class Launcher {
 			System.out.println();
 		}
 	}
-	
+
 	public void manualShoot(Joystick joyStick){
 		if (joyStick.getRawButton(11)){ //shooting from the base of the tower
 			shoot(1.0, 1.0);
-			Timer.delay(1.0);
-			loadTalon.set(1.0);
-			Timer.delay(0.5);
-			System.out.println("shoot full speed (bottom of tower)");
+			//System.out.println("shoot full speed (bottom of tower)");
 		}
 		else if(joyStick.getRawButton(12)){ //shooting from green line
 			shoot(1.0, 0.75);
-			System.out.println("shoot lower speed (green line)");
-		}
-		else if (joyStick.getRawButton(9)){
-			lowerShootTalon.set(1.0);
-		}
-		else if (joyStick.getRawButton(10)){
-			lowerShootTalon.set(1.0);
-			Timer.delay(0.5);
-			loadTalon.set(-0.5);
-			Timer.delay(0.3);
-		}
-		else if (joyStick.getRawButton(7)){
-			lowerShootTalon.set(1.0);
-			Timer.delay(0.5);
-			loadTalon.set(-0.5);
-			Timer.delay(0.2);
-			loadTalon.set(0.0);
-			shoot(1.0, 1.0);
-			Timer.delay(1.0);
-			loadTalon.set(1.0);
-			Timer.delay(0.5);
+			//System.out.println("shoot lower speed (green line)");
 		}
 		else {
 			shoot(0.0, 0.0);
-			loadTalon.set(0.0);
 		}
 	}
-	
+
 	public void shoot(double lowerWheel, double upperWheel){
 		lowerShootTalon.set((Math.abs(lowerWheel))*-1.0); //makes sure lower wheel value is always negative
-		upperShootTalon.set((Math.abs(upperWheel))*-1.0);
+		upperShootTalon.set(upperWheel);
 	}
-	
+
 	public void autoShoot(double papasVisionDistance, double error){
 		double lower = 0, upper = 0;
 		//put in algorithm to calculate upper and lower wheel speeds
 		shoot(lower, upper);
 	}
-	
+
 	/*
-	
-	//method created to get upper and lower bound values of the servo when rotating shooter is attached to robot
-	//with this method we can find the exact degree range the servo has when shooter mounted between the arms
-	public void testTurn(Joystick joyStick){
-		double deg = turnServo.getAngle();
-		if ((joyStick.getPOV()==(0))){ //right on shooter, up on pov, positive servo angle val
-			deg += 5.0;
-		}
-		else if (joyStick.getPOV()==(180)){
-			deg -= 5.0;
-		}
-		turnServo.setAngle(deg);
-		System.out.println("Servo angle is: " + turnServo.getAngle());
-	}
-	
-	public void turn(double angle){
-		if (angle > max){
-			System.out.println("Desired target too far right of shooter range. Turn robot right.");
-		}
-		else if (angle < min){
-			System.out.println("Desired target too far left of shooter range. Turn robot left.");
-		}
-		else {
-			turnServo.setAngle(angle);
-		}
-		System.out.println("Servo angle is: " + turnServo.getAngle());
-	}
-	
-	public void manualTurn0(Joystick joyStick){
-		double deg = turnServo.getAngle();
-		if (joyStick.getPOV()==(90)){ //turns shooter right
-			deg += 5.0;
-			turn(deg);
-		}
-		else if (joyStick.getPOV()==(270)){ //turns shooter left
-			deg -= 5.0;
-			turn(deg);
-		}
-	}
-	
-	//papas vision angle: RIGHT NEGATIVE, LEFT POSITIVE
-	public void autoTurn0(double papasVisionAngle, double error){ //start error value at about 3 degrees
-		double ang = turnServo.getAngle();
-		//if papas vision angle is left of tower, need to move servo right, if papas vision angle is right of tower, need to move servo left
-		if (papasVisionAngle > (0 + error)){ //left of tower, need to move servo right
-			turn(ang + papasVisionAngle);
-		}
-		else if (papasVisionAngle <(0 - error)){//right of tower, need to move servo left
-			turn(ang - papasVisionAngle);
-		}
-	}*/
-	
+
+        //method created to get upper and lower bound values of the servo when rotating shooter is attached to robot
+        //with this method we can find the exact degree range the servo has when shooter mounted between the arms
+        public void testTurn(Joystick joyStick){
+                double deg = turnServo.getAngle();
+                if ((joyStick.getPOV()==(0))){ //right on shooter, up on pov, positive servo angle val
+                        deg += 5.0;
+                }
+                else if (joyStick.getPOV()==(180)){
+                        deg -= 5.0;
+                }
+                turnServo.setAngle(deg);
+                //System.out.println("Servo angle is: " + turnServo.getAngle());
+        }
+
+        public void turn(double angle){
+                if (angle > max){
+                        System.out.println("Desired target too far right of shooter range. Turn robot right.");
+                }
+                else if (angle < min){
+                        System.out.println("Desired target too far left of shooter range. Turn robot left.");
+                }
+                else {
+                        turnServo.setAngle(angle);
+                }
+                System.out.println("Servo angle is: " + turnServo.getAngle());
+        }
+
+        public void manualTurn0(Joystick joyStick){
+                double deg = turnServo.getAngle();
+                if (joyStick.getPOV()==(90)){ //turns shooter right
+                        deg += 5.0;
+                        turn(deg);
+                }
+                else if (joyStick.getPOV()==(270)){ //turns shooter left
+                        deg -= 5.0;
+                        turn(deg);
+                }
+        }
+
+        //papas vision angle: RIGHT NEGATIVE, LEFT POSITIVE
+        public void autoTurn0(double papasVisionAngle, double error){ //start error value at about 3 degrees
+                double ang = turnServo.getAngle();
+                //if papas vision angle is left of tower, need to move servo right, if papas vision angle is right of tower, need to move servo left
+                if (papasVisionAngle > (0 + error)){ //left of tower, need to move servo right
+                        turn(ang + papasVisionAngle);
+                }
+                else if (papasVisionAngle <(0 - error)){//right of tower, need to move servo left
+                        turn(ang - papasVisionAngle);
+                }
+        }*/
+
 	//all the new shit below
-	
+
 	public void manualTurn(Joystick joyStick){
-		if (joyStick.getPOV()==(90))
+		if (joyStick.getPOV()==(90)){
 			spinTalon.set(1.0);
-		else if (joyStick.getPOV()==(270))
+		}
+		else if (joyStick.getPOV()==(270)){
 			spinTalon.set(-1.0);
-		//else if (joyStick.getTrigger()){
-			//spinTalon.set(1.0);
-			//Timer.delay(0.5);
-			//spinTalon.set(0.0);
-		//}	
-		else
+		}
+		else if (joyStick.getTrigger()){
+			spinTalon.set(1.0);
+			Timer.delay(0.5);
 			spinTalon.set(0.0);
+		}
+		else{
+			spinTalon.set(0.0);
+		}
 	}
 	
+	public double calcAngle(double camAngle, double camDistance){
+		double outerLength = camDistance * (Math.cos(Math.toRadians(camAngle)));
+		double innerLength = 12.75;
+		double outerHeight = camDistance * (Math.sin(Math.toRadians(camAngle))); 
+		double length = outerLength + innerLength;
+		double newAng;
+		if (length != 0){ 
+			newAng = Math.toDegrees(Math.atan(outerHeight/length));
+		}
+		else{
+			newAng = camAngle;
+		}
+		return newAng;
+	}
+	
+	public double calcDistance(double camAngle, double camDistance){
+		double outerLength = camDistance * (Math.cos(Math.toRadians(camAngle)));
+		double innerLength = 12.75;
+		double outerHeight = camDistance * (Math.sin(Math.toRadians(camAngle))); 
+		double length = outerLength + innerLength;
+		double dist = Math.sqrt((Math.pow(length, 2))+Math.pow(outerHeight,2));
+		return dist;
+	}
+
 	//papas vision angle: RIGHT NEGATIVE, LEFT POSITIVE
-	public void autoTurn(double papasVisionAngle, double error){ //start error value at about 3 degrees
+	public void autoTurn(double papasVisionAngle, double papasVisionDistance, double error){ //start error value at about 3 degrees
+		double newAng = calcAngle(papasVisionAngle, papasVisionDistance);
 		double angRate = 53.2327; //value calculated by testing distance of chord travelled in .5 sec, could be made more accurate by more tests
 		//if papas vision angle is left of tower, need to move servo right, if papas vision angle is right of tower, need to move servo left
-		double angle = Math.abs(papasVisionAngle);
+		double angle = Math.abs(newAng);
 		double time = (angle/angRate);
-		if (papasVisionAngle > (0 + error)){ //left of tower, need to move servo right
+		if (newAng > (0 + error)){ //left of tower, need to move servo right
 			spinTalon.set(1.0);
 			Timer.delay(time);
 			spinTalon.set(0.0);
 		}
-		else if (papasVisionAngle <(0 - error)){//right of tower, need to move servo left
+		else if (newAng <(0 - error)){//right of tower, need to move servo left
 			spinTalon.set(1.0);
 			Timer.delay(time);
 			spinTalon.set(0.0);
 		}
 	}
-	
 }
--- superawesomerobot2k16/src/org/usfirst/frc/team1759/robot/Robot.java
+++ superawesomerobot2k16/src/org/usfirst/frc/team1759/robot/Robot.java
@@ -1,21 +1,22 @@
 package org.usfirst.frc.team1759.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-//import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.command.Command; //uncomment for autonomous
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.CameraServer;
+
 //import edu.wpi.first.wpilibj.AnalogPotentiometer;
 
-
 import edu.wpi.first.wpilibj.Ultrasonic;
+
+import java.lang.Thread;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -25,162 +26,146 @@ import edu.wpi.first.wpilibj.Ultrasonic;
  * directory.
  */
 public class Robot extends IterativeRobot {
-    final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
-    String autoSelected;
-    SendableChooser chooser;
-	
-    RobotDrive myRobot;
-    Launcher launcher;
-    //Climber climber; //uncomment for climbing
-    Joystick leftStick;
-    Joystick rightStick;
-    Joystick shootStick;
-    CANTalon canTalon0;
-    CANTalon canTalon1;
-    CANTalon canTalon2;
-    CANTalon canTalon3;
-    
-    CommandGroup autoCom; //uncomment for autonomous
-    //auto -- 60% for 5 seconds
-    CameraServer server;
-    
-    //AnalogPotentiometer pot; //for testing purposes
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-    	//enable the camera
-    	server = CameraServer.getInstance();
-        server.setQuality(100);
-        server.startAutomaticCapture("cam0");
-    	
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("My Auto", customAuto);
-        SmartDashboard.putData("Auto choices", chooser);												
-        
-        canTalon0 = new CANTalon(0); 
-    	canTalon1 = new CANTalon(1);
-    	canTalon2 = new CANTalon(2);
-    	canTalon3 = new CANTalon(7);
-    	
-    	//output for talons was reversed so this inverts them
-    	canTalon0.setInverted(true);
-    	//canTalon1.setInverted(true);
-    	canTalon2.setInverted(true);
-    	canTalon3.setInverted(true);
-    	
-    	//front left, back left, front right, back right
-        myRobot = new RobotDrive(canTalon0, canTalon1, canTalon2, canTalon3);
-        
-        //load talon port (cantalon), lower shoot talon port(cantalon), upper shoot talon port(cantalon)
-        launcher = new Launcher(8,5,6);
-        leftStick = new Joystick(0);
-        rightStick = new Joystick(1);
-        shootStick = new Joystick(2);
-        
-        //talonPortActuator1 (cantalon) ,talonPortActuator2 (cantalon),lowerlimitswitch1port, lowerlimitswitch2port, upperlimitswitch1port, upperlimitswitch2port
-        //climber = new Climber(7,4,0,1); //uncomment for climbing
-        
-        autoCom = new AutoCom(myRobot, launcher); //uncomment for autonomous
-        
-        //pot = new AnalogPotentiometer(3); //for testing purposes
-    }
-    
+	final String defaultAuto = "Default";
+	final String customAuto = "My Auto";
+	String autoSelected;
+	SendableChooser chooser;
+
+	RobotDrive myRobot;
+	Launcher launcher;
+	Joystick leftStick;
+	Joystick rightStick;
+	Joystick shootStick;
+	CANTalon canTalon0;
+	CANTalon canTalon1;
+	CANTalon canTalon2;
+	CANTalon canTalon3;
+	CANTalon canTalon4;
+	CANTalon canTalon6;
+	CANTalon canTalon7;
+	Solenoid solenoidFire;
+
+	CommandGroup autoCom;
+	boolean camOn;
+
+	Ultrasonic ultrasanic;
+	// CameraServer server;
+
+	// AnalogPotentiometer pot; //for testing purposes
 	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
 	 */
-    public void autonomousInit() {
-    	autoSelected = (String) chooser.getSelected();
-//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
+	public void robotInit() {
+		// enable the camera
+		/*
+		 * server = CameraServer.getInstance(); server.setQuality(100);
+		 * server.startAutomaticCapture("cam0");
+		 */
+
+		chooser = new SendableChooser();
+		chooser.addDefault("Default Auto", defaultAuto);
+		chooser.addObject("My Auto", customAuto);
+		SmartDashboard.putData("Auto choices", chooser);
+		
+		solenoidFire = new Solenoid(1);
+
+		canTalon0 = new CANTalon(0);//not used
+		canTalon1 = new CANTalon(1);//not used
+		canTalon2 = new CANTalon(2);//Back left
+		canTalon3 = new CANTalon(3);//Front Right
+		canTalon4 = new CANTalon(4);//Back right
+		canTalon6 = new CANTalon(6);//Cannon Y
+		canTalon7 = new CANTalon(7);//Front left
+		
+		// output for talons was reversed so this inverts them
+		canTalon0.setInverted(false);
+		canTalon1.setInverted(false);
+		canTalon2.setInverted(false);
+		canTalon3.setInverted(false);
+		canTalon4.setInverted(false);
+		canTalon7.setInverted(false);
+
+		camOn = false;
+
+		// front left, back left, front right, back right
+		myRobot = new RobotDrive(canTalon7, canTalon2, canTalon1, canTalon3);
+
+		// load talon port (cantalon), lower shoot talon port(cantalon), upper
+		// shoot talon port(cantalon)
+		leftStick = new Joystick(0);
+		rightStick = new Joystick(2);
+		
+
+		// talonPortActuator1 (cantalon) ,talonPortActuator2
+		// (cantalon),lowerlimitswitch1port, lowerlimitswitch2port,
+		// upperlimitswitch1port, upperlimitswitch2port
+
+		autoCom = new AutoCom(myRobot, launcher);
+
+		// pot = new AnalogPotentiometer(3); //for testing purposes
+	}
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the
+	 * switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	public void autonomousInit() {
+		autoSelected = (String) chooser.getSelected();
+		// autoSelected = SmartDashboard.getString("Auto Selector",
+		// defaultAuto);
 		System.out.println("Auto selected: " + autoSelected);
-		autoCom.start(); //uncomment to use autonomous
-    }
+		autoCom.start();
+	}
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-    	switch(autoSelected) {
-    	case customAuto:
-        //Put custom auto code here   
-            break;
-    	case defaultAuto:
-    	default:
-    	//Put default auto code here
-            break;
-    	}
-    	Scheduler.getInstance().run(); //uncomment to use autonomous
-    }
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+		switch (autoSelected) {
+		case customAuto:
+			// Put custom auto code here
+			break;
+		case defaultAuto:
+		default:
+			// Put default auto code here
+			break;
+		}
+		Scheduler.getInstance().run();
+	}
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-    	myRobot.tankDrive(leftStick, rightStick); //two joystick tank drive system
-    	launcher.manualShoot(shootStick); //uses throttle on both joysticks to control shooting system
-    	launcher.load(shootStick, 1.0, 0.5); //uses buttons 5 and 3 on right joystick to run loading motor
-    	launcher.manualTurn(shootStick);
-    	//climber.climb(shootStick); //uncomment for climbing
-    	//System.out.println("POT " + (pot.get())); //for4 testing purposes
-    	//System.out.println(ultrasanic.getRangeInches());
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    
-    }
-    
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		myRobot.tankDrive(leftStick, rightStick);// two joystick tank drive
+		//myRobot.setMaxOutput(0.5);
+		canTalon6.set((leftStick.getThrottle()-.5)/-5);
+		//cannon angle 
+		boolean fire;
+		fire = leftStick.getTrigger();
+		//gets trigger state
+		if(fire=true){
+			solenoidFire.set(true);
+			//trigger solenoid
+		}
+		solenoidFire.set(false);
+		//reset solenoid
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+		
+	}
+
 }
-
-/*
- * TO DO WEDNESDAY:
- * - fix driver station
- * - camera shit
- * - set up potentiometer stuff
- * - test continuous servo
- * - create methods to use feedback from papas vision
- */
-
-/*
- * TO DO AT COMP:
- * put on potentiometers
- * put on camera
- * remount electronics boards
- *
- */
-
-/*
- * shit to do
- * - test all manual controls
- * - switch loading back to full power talon
- * - get camera images on
- * - test and practice w autonomous
- *  
- * 
- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
